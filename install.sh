1bedcee57c165c6055c33a6be95efce99bdc37ec
Teitei011 <stefantleal14@gmail.com>
2021-12-15 21:33:28 -0300

    Modifications that change the config file

--- install.sh
+++ install.sh
@@ -2,6 +2,17 @@ echo "Updating all the programs"
 
 sudo pacman -Suy
 
+echo "Editing pacman.conf"
+sudo echo "ParallelDownloads = 5" >> /etc/pacman.conf
+
+echo "DRI_PRIME configuration"
+sudo echo "DRI_PRIME=1" >> /etc/enviroment
+
+echo "Putting neofetch into bash/zsh"
+echo "neofetch" >> .bashrc
+echo "neofetch" >> .zshr
+
+
 echo "Instaling go and git..."
 
 sudo pacman -S go git 
@@ -15,7 +26,7 @@ echo "Installing yay..."
 makepkg -si 
 
 echo "Installing all other programs..."
-yay -S btop htop cpu-x obsidian telegram-desktop brave-browser nextcloud-client aur/jellyfin-media-player stacer-git easyeffects noisetorch-git tlp community/gnome-multi-writer extra/transmission-qt
+yay -S btop htop cpu-x obsidian telegram-desktop brave-browser nextcloud-client aur/jellyfin-media-player stacer-git easyeffects noisetorch-git tlp community/gnome-multi-writer extra/transmission-qt vim
 
 
 rm -r yay 
